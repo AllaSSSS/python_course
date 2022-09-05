b72f40a317acde054e5a6acbea7d023c508822cf
alla <fi-alka@yandex.ru>
2022-09-05 21:24:47 +0300

    сделала половину 4-го задания, создала файл с позициями

--- python_homework/sem2_homework/hw4.py
+++ python_homework/sem2_homework/hw4.py
@@ -1,3 +1,12 @@
 #Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 #Найдите произведение элементов на указанных позициях. 
 #Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import randint
+
+N = int(input('Введите число: '))
+numbers = []
+for i in range(N):
+    numbers.append(randint(-N, N+1))
+print(numbers)
+
