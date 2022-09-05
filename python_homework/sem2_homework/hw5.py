d7a0d337b582c1c5fc7041de15412ae4ac1fd839
alla <fi-alka@yandex.ru>
2022-09-05 22:50:44 +0300

    task5 with both random and specified lists

--- python_homework/sem2_homework/hw5.py
+++ python_homework/sem2_homework/hw5.py
@@ -1,1 +1,15 @@
 #Реализуйте алгоритм перемешивания списка.
+
+import random
+from random import randint
+list_1 = []
+for i in range(10):
+    list_1.append(randint(-100, 100))
+print(list_1)
+random.shuffle(list_1)
+print(list_1)
+
+list_2 = ['каждый', 'охотник', 'желает', 'знать', 'где', 'сидит', 'фазан']
+print(list_2)
+random.shuffle(list_2)
+print(list_2)
