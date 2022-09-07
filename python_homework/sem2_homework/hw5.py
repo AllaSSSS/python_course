52debaf66028da47aed047e0a279a36ae9c6386e
alla <fi-alka@yandex.ru>
2022-09-07 15:03:04 +0300

    task5 final update

--- python_homework/sem2_homework/hw5.py
+++ python_homework/sem2_homework/hw5.py
@@ -3,7 +3,7 @@
 import random
 from random import randint
 list_1 = []
-for i in range(10):
+for i in range(5):
     list_1.append(randint(-100, 100))
 print(list_1)
 
@@ -12,10 +12,7 @@ for i in range (len(list_1) - 1, 1, -1):
     list_1[i], list_1[j] = list_1[j], list_1[i]
 print(list_1)
 
-# random.shuffle(list_1)
-# print(list_1)
-
-# list_2 = ['каждый', 'охотник', 'желает', 'знать', 'где', 'сидит', 'фазан']
-# print(list_2)
-# random.shuffle(list_2)
-# print(list_2)
+list_2 = ['каждый', 'охотник', 'желает', 'знать', 'где', 'сидит', 'фазан']
+print(list_2)
+random.shuffle(list_2)
+print(list_2)
