af2f44f447a279c015111941a3ad019cd95db76d
alla <fi-alka@yandex.ru>
2022-09-11 18:46:12 +0300

    task5

--- python_homework/sem3_homework/task5.py
+++ python_homework/sem3_homework/task5.py
@@ -3,12 +3,20 @@
 # - для k = 8 список будет выглядеть так:
 #  [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
 
-double Fibonacci (int n)
-{
-    if (n == 1 || n == 2) return 1;
-    else return Fibonacci(n - 1) + Fibonacci (n - 2);
-}
+def Fibo(n):
+    if n < 0:
+        if n % 2 != 0: 
+            return Fibo(-n) 
+        else:
+            return -Fibo(-n)
+    if n < 2: return n
+    else: 
+        return Fibo(n-2) + Fibo(n-1)
 
-for (int i = 1; i < 50; i ++)
-{
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+def ListFibo(k):
+    arr = []
+    for i in range(-k, k+1):
+        arr.append(str(Fibo(i)))
+    return arr
+
+print(', '.join(ListFibo(8)))
