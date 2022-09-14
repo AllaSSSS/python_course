e5aef8e2cd435685d359822161d980a3c321a706
alla <fi-alka@yandex.ru>
2022-09-14 18:05:33 +0300

    task1, 2

---
+++ python_homework/sem4_homework/NOK.py
@@ -0,0 +1,13 @@
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+def Find_GCD(a, b): # GCD = greatest common divisor
+    while a != b:
+        print(a, " ", b)
+        if a > b: a -= b
+        else: b -= a
+    return a
+
+print("Наименьшее общее кратное чисел", a, "и", b, "равно", a * b // Find_GCD(a, b))
+
