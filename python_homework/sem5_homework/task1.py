354ec8a2d75eec3396664816fa1630fe24beaeca
alla <fi-alka@yandex.ru>
2022-09-18 21:28:12 +0300

    task 2

--- python_homework/sem5_homework/task1.py
+++ python_homework/sem5_homework/task1.py
@@ -3,4 +3,4 @@ import re
 def remove_words_with_substring(sentence, substring):
     return re.sub(r"\s*[а-яА-Я\w]*" + substring + r"[а-яА-Я\w]*", "", sentence)
 
-print(remove_words_with_substring("Мы неабв очень любим Питон иабв Джавуабв!", "абв"))
+print(remove_words_with_substring("Мы неабв очень любим Питон     \n иабв Джавуабв!", "абв"))
